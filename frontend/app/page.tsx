e02e444cdc0af68049513c64f3253d63d205179a
tyagi-anurag <anuragtyagi157@gmail.com>
2025-11-30 22:15:59 +0530

    Updated the backend url

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -17,7 +17,7 @@ export default function MotoMindUI() {
   // ---------------------------------------------------------
   // 🔧 CONFIGURATION: PASTE YOUR DEPLOYED BACKEND URL HERE
   // ---------------------------------------------------------
-  const BACKEND_URL = "http://127.0.0.1:8000"; // <--- CHANGE THIS TO YOUR RENDER/CLOUD RUN URL
+  const BACKEND_URL = "https://motomind-backend.onrender.com"; // <--- CHANGE THIS TO YOUR RENDER/CLOUD RUN URL
   // Example: const BACKEND_URL = "https://motomind-backend-xp2z.onrender.com";
   // ---------------------------------------------------------
 
